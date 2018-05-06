2072caef4e484d77053d4bc252dab32e8c31d080
Kavya Vemula <kavya.vemula1@marist.edu>
2018-05-06 00:46:02 -0400

    Added a functionality where you can display results by pressing Enter/Return button

--- app.js
+++ app.js
@@ -30,13 +30,18 @@ weatherApp.service('cityService', function() {
 
 
 //Defining Controllers
-weatherApp.controller('homeController',['$scope','cityService', function($scope, cityService) {
+weatherApp.controller('homeController',['$scope', '$location', 'cityService', function($scope, $location, cityService) {
     
     $scope.city = cityService.city;
     
     $scope.$watch('city', function() {
         cityService.city = $scope.city;
     });
+    
+    //Function for the "Get Forecast" button
+    $scope.submit = function() {
+        $location.path("/forecast");
+    };
 
 }]);
 
